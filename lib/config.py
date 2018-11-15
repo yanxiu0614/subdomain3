7548ec245ad47c34e7227163fb8306cd48f4bbc7
yanxiu <654494146@qq.com>
2018-11-15 11:07:40 +0800

    Reduce default rate

--- lib/config.py
+++ lib/config.py
@@ -11,13 +11,13 @@ waiting_fliter_ip = [
 # 速度分为三种模式，可以根据以下配置进行调节
 
 # high
-high_segment_num = 20000  # 程序采用逐量放到内存爆破，以减少内存占用。该设置会改变每次的读取量
+high_segment_num = 10000  # 程序采用逐量放到内存爆破，以减少内存占用。该设置会改变每次的读取量
 
 # medium
-medium_segment_num = 15000
+medium_segment_num = 5000
 
 # low
-low_segment_num = 10000
+low_segment_num = 2000
 
 # 设置一个ip出现的最多次数,后续出现将被丢弃
 ip_max_count = 30
